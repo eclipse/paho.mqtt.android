fcaabd34dfebc08b2136cce93687ad3ffbd9e0b2
Ian Craggs <icraggs@uk.ibm.com>
2015-05-29 15:23:51 +0100

    Add more tests for per subscription messsage handlers, and move match method to MqttTopic
    Bug: 466579

--- org.eclipse.paho.android.service/src/org/eclipse/paho/android/service/MqttAndroidClient.java
+++ org.eclipse.paho.android.service/src/org/eclipse/paho/android/service/MqttAndroidClient.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 1999, 2014 IBM Corp.
+ * Copyright (c) 1999, 2015 IBM Corp.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -9,6 +9,9 @@
  *    http://www.eclipse.org/legal/epl-v10.html
  * and the Eclipse Distribution License is available at 
  *   http://www.eclipse.org/org/documents/edl-v10.php.
+ *   
+ *   Ian Craggs - Per subscription message handlers bug 466579
+ *   
  */
 package org.eclipse.paho.android.service;
 
