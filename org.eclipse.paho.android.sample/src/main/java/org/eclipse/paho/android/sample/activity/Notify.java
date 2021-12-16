cbacb174be178c818e7f9a1e9d3346a11821138d
samekawaii7 <samekawaii7@gmail.com>
2021-12-16 13:54:08 +0900

    Fix jitpack build.

--- org.eclipse.paho.android.sample/src/main/java/org/eclipse/paho/android/sample/activity/Notify.java
+++ org.eclipse.paho.android.sample/src/main/java/org/eclipse/paho/android/sample/activity/Notify.java
@@ -3,11 +3,11 @@
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
- * and Eclipse Distribution License v1.0 which accompany this distribution. 
+ * and Eclipse Distribution License v1.0 which accompany this distribution.
  *
- * The Eclipse Public License is available at 
+ * The Eclipse Public License is available at
  *    http://www.eclipse.org/legal/epl-v10.html
- * and the Eclipse Distribution License is available at 
+ * and the Eclipse Distribution License is available at
  *   http://www.eclipse.org/org/documents/edl-v10.php.
  */
 package org.eclipse.paho.android.sample.activity;
@@ -54,7 +54,7 @@ class Notify {
 
     //build the pending intent that will start the appropriate activity
     PendingIntent pendingIntent = PendingIntent.getActivity(context,
-            0, intent, 0);
+            0, intent, PendingIntent.FLAG_IMMUTABLE);
 
     //build the notification
     Builder notificationCompat = new Builder(context);
